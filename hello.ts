aab3785ecb647fe4c12488a51e63bd117f0f70f3
Freewind <nowindlee@gmail.com>
2020-10-15 14:39:59 +0800

    update versions to latest

--- hello.ts
+++ hello.ts
@@ -1,9 +1,10 @@
-import ts = require('typescript');
+import ts from 'typescript';
 
 const code = "enum { x = 1 }"
 const sourceFile = ts.createSourceFile('x.ts', code, ts.ScriptTarget.Latest, true);
 
 let indent = 0;
+
 function print(node: ts.Node) {
   console.log(new Array(indent + 1).join(' ') + ts.SyntaxKind[node.kind]);
   indent++;
